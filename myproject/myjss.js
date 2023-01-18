80e8ec78f67dd31cce7a02c3c7e26a1472f9a743
Ankit <ankitkumarbarsi007@hgamil.com>
2023-01-17 22:38:20 -0500

    first commit

---
+++ myproject/myjss.js
@@ -0,0 +1,27 @@
+const todoForm=document.querySelector(".form-todo");
+const todoinput=document.querySelector(".form-todo input[type='text']")
+const todolist=document.querySelector(".todo-list");
+
+todoForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const newtodotext=todoinput.value;
+    todoinput.value="";
+    const newli=document.createElement("li");
+    const newliinner=`<span class"text">${newtodotext}</span>
+    <div class="todo-buttons">
+        <button class="todo-btn done">Done</button>
+        <button class="todo-btn remove">Remove</button></div>`
+newli.innerHTML=newliinner;
+todolist.append(newli);
+todoinput.value="";
+todolist.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("remove")){
+        
+    }
+    if(e.target.classList.contains("done")){
+const lispan=e.target.parentNode.previousElementSibling;
+lispan.style.textDecoration="line-through";
+    }
+})
+
+})
